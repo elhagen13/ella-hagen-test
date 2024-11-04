be567d23e1380416cea4c9a0b192c6880a56f590
Ella Hagen <elhagen@calpoly.edu>
2024-11-04 15:17:18 -0800

    feature: added my page

---
+++ src/app/ella/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <div style={{position:'absolute', left:'0px', top:'0px', width: "100vw", height:"100vh", backgroundColor: '#5d8c4f', overflow:'hidden'}}>
+      <div style={{ color: "#566353", fontSize: "46px", width: "100%", height: "100%", textAlign:'center'}}>
+          Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!Test Page!
+      </div>
+    </div>
+  );
+}
